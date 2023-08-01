648ab34988d3dde652228b177d31172a7409417a
dottomm <dottomm@gmail.com>
2023-08-01 08:09:36 -0700

    edits to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,14 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()]
+  integrations: [compress(), mdx(), tailwind(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkPlugins: [remarkToc],
+      rehypePlugins: [rehypeMinifyHtml],
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false,
+    })
+  ]
 });
